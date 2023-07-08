6b14dd9af8ccccfdb42de48a618e46835b90fad0
qhqjq1890 <83694782+qhqjq1890@users.noreply.github.com>
2023-07-09 00:24:55 +0900

    footer
    footer추

--- main.js
+++ main.js
@@ -25,6 +25,24 @@ function getDate() {
   ).innerText = `${year}년 ${month}월 ${dt}일 ${daykor}요일`;
 }
 
+async function getWeather() {
+  try {
+    const response = await fetch(
+      "http://api.weatherapi.com/v1/current.json?key=ed243edc132d40e7b00151036230807&q=Seoul&lang=ko"
+    );
+    const data = await response.json();
+    console.log(data.current.condition.icon);
+    const weatherElements = document.getElementsByClassName("weather");
+    Array.from(weatherElements).forEach((element) => {
+      element.style.display = "flex";
+      element.innerHTML = `<img src="${data.current.condition.icon}" width=20px height=20px alt="weather"> <p>${data.current.condition.text}</p>`;
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 getClock();
 getDate();
+getWeather();
 setInterval(getClock, 1000);
